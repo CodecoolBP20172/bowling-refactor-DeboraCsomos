c59081e622454f43b715c04114544b579743be76
Debora Csomos <csomidebi@gmail.com>
2017-07-28 14:46:09 +0200

    added function which counts the score for strike or spare

--- bowling.py
+++ bowling.py
@@ -27,6 +27,18 @@ def frame_counting(frame, in_first_half, char):
     return frame, in_first_half
 
 
+def score_for_strike_and_spare(game, i, char, result):
+    score = 0
+    next_roll = game[i + 1]
+    next_to_next_roll = game[i + 2]
+    score += get_value(next_roll)
+    if is_strike(char):
+        if is_spare(next_to_next_roll):
+            score += get_value(next_to_next_roll) - get_value(next_roll)
+        else:
+            score += get_value(next_to_next_roll)
+    return score
+
 def score(game):
     result = 0
     frame = 1
@@ -38,14 +50,7 @@ def score(game):
         elif is_number(char) or is_strike(char):
             result += get_value(char)
         if frame < 10 and is_strike(char) or is_spare(char):
-            next_roll = game[i + 1]
-            next_to_next_roll = game[i + 2]
-            result += get_value(next_roll)
-            if is_strike(char):
-                if is_spare(next_to_next_roll):
-                    result += get_value(next_to_next_roll) - get_value(next_roll)
-                else:
-                    result += get_value(next_to_next_roll)
+            result += score_for_strike_and_spare(game, i, char, result)
         last = get_value(char)
         frame, in_first_half = frame_counting(frame, in_first_half, char)
     return result
