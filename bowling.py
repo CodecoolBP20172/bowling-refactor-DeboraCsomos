c9a4385f7c7a31251a41b2749b60bc0780c298ee
Debora Csomos <csomidebi@gmail.com>
2017-07-28 15:06:09 +0200

    replaced i with roll for mor eunderstandable naming

--- bowling.py
+++ bowling.py
@@ -15,6 +15,7 @@ def is_number(char):
         return True
     return False
 
+
 def frame_counting(frame, in_first_half, char):
     if in_first_half:
         in_first_half = False
@@ -27,10 +28,10 @@ def frame_counting(frame, in_first_half, char):
     return frame, in_first_half
 
 
-def score_for_strike_and_spare(game, i, char, result):
+def score_for_strike_and_spare(game, roll, char, result):
     score = 0
-    next_roll = game[i + 1]
-    next_to_next_roll = game[i + 2]
+    next_roll = game[roll + 1]
+    next_to_next_roll = game[roll + 2]
     score += get_value(next_roll)
     if is_strike(char):
         if is_spare(next_to_next_roll):
@@ -39,18 +40,20 @@ def score_for_strike_and_spare(game, i, char, result):
             score += get_value(next_to_next_roll)
     return score
 
+
 def score(game):
     result = 0
     frame = 1
     in_first_half = True
-    for i in range(len(game)):
-        char = game[i]
+    rolls = range(len(game))
+    for roll in rolls:
+        char = game[roll]
         if is_spare(char):
             result += get_value(char) - last
         elif is_number(char) or is_strike(char):
             result += get_value(char)
         if frame < 10 and is_strike(char) or is_spare(char):
-            result += score_for_strike_and_spare(game, i, char, result)
+            result += score_for_strike_and_spare(game, roll, char, result)
         last = get_value(char)
         frame, in_first_half = frame_counting(frame, in_first_half, char)
     return result
